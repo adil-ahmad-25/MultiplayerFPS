adc685c79a29118eb35b859a05937e73c3a9fd9b
adil-ahmad-25 <adilahmad.mris@gmail.com>
2024-11-23 15:23:08 +0400

    EndofVid3

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -2,15 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using TMPro;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int health;
 
+    [Header("UI")]
+    public TextMeshProUGUI healthText;
+
     [PunRPC]
     public void TakeDamage(int damage)
     {
         health -= damage;
+
+        healthText.text = health.ToString();
 
         if(health <= 0)
         {
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,51 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public int damage;
+
+    public float fireRate;
+
+    public Camera playerCamera;
+
+    [Header("VFX")]
+    public GameObject hitVFX;
+
+    private float nextFire;
+
+
+    void Update()
+    {
+        if (nextFire > 0)
+        {
+            nextFire -= Time.deltaTime;
+        }
+        
+        if (Input.GetButton("Fire1") && nextFire <= 0)
+        {
+            nextFire = 1 / fireRate;
+            
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        Ray ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray.origin,ray.direction, out hit, 100f))
+        {
+            PhotonNetwork.Instantiate(hitVFX.name, hit.point, Quaternion.identity);
+            
+            if (hit.transform.gameObject.GetComponent<PlayerHealth>())
+            {
+                hit.transform.gameObject.GetComponent<PhotonView>().RPC("TakeDamage", RpcTarget.All, damage);
+            }
+        }
+    }
+}
